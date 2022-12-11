2cbc639e6aa0c9974b15818dcc06d36e74488270
Igor Baranov <eltery@mail.ru>
2022-12-11 12:50:06 +0300

    User array method done

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ int result = NumberPower(userNumber, userPower);
 Console.WriteLine($"{userNumber} в степени {userPower} равно {result}");
 */
 
-
+/*
 int SumOfDigits (int number)
 {
     int sum = 0;
@@ -41,6 +41,38 @@ int userNumber = Convert.ToInt32(Console.ReadLine());
 
 int result = SumOfDigits(userNumber);
 Console.WriteLine($"Сумма цифр числа {userNumber} равна {result}");
+*/
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[size];
+    int i = 0;
+    while (i < size)
+    {
+        Console.Write($"Введите {i+1}й элемент массива: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+        i++;
+    }
+    
+    return array;
+}
+
+void ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    {
+      Console.Write(array[i] + " ");
+    }
+}
+
+
+Console.Write("Введите число элементов массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+
+int[] myArray = CreateArray(length);
+ShowArray(myArray);
+
+
 
 
 
