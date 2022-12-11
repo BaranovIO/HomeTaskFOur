e8bc840ad59f8f15b6a69f683f880ab0a289f358
Igor Baranov <eltery@mail.ru>
2022-12-11 10:55:39 +0300

    Number power task done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int NumberPower (int number, int power)
+{
+    int product = 1;
+   // int currentPower = 1;
+     for ( int currentPower = 1; currentPower <= power; currentPower++)
+     {
+         product = product * number;
+     }
+   
+    return product;
+}
+
+Console.Write("Введите число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите степень, в которую нужно возвести число: ");
+int userPower = Convert.ToInt32(Console.ReadLine());
+
+int result = NumberPower(userNumber, userPower);
+Console.WriteLine($"{userNumber} в степени {userPower} равно {result}");
